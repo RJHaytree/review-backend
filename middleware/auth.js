9beaf09949b216ed9b483f021b3f00e32e6901e7
RJHaytree <rjhaytree@gmail.com>
2022-05-15 16:03:02 +0100

    Log

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ const utility = require('../utilities/utility');
 
 module.exports = async (req, res, next) => {
     let token = req.header('Auth-Token');
+    console.log('Token:' + token);
 
     try {
         if (!token) {
